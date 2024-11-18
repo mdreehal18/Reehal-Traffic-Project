e6c5091f159d643548baf39b834fda9add75bf0a
M Mohammad Reehal <reehalmd@gmail.com>
2024-11-18 18:36:33 +0530

    Add files via upload

---
+++ TP-task1.py
@@ -0,0 +1,21 @@
+import math
+#task-1
+print("Hello World")
+#task-2
+for i in range(5):
+    print(i)
+i=1
+if(i<3):
+    print(i)
+else:
+    print("Incorrect value") 
+ #task-3
+def my_self(name):  
+    print("Myself"+name)   
+my_self("Reehal") 
+#task-4
+
+
+x = math.sqrt(64)
+
+print(x)     
